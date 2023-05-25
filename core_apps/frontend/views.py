10856d24c14a6f79c1eddede72746b479700e369
Maina Wanjau <mwanjau@safaricom.co.ke>
2023-05-25 08:09:32 +0300

    contact form working well

--- core_apps/frontend/views.py
+++ core_apps/frontend/views.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def contact(request):
+
+    context = {}
+
+    return render(request, 'contact/contactform.html', context)
