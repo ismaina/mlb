257c5b1e959e5d30f439255f7bead03d341f5bcd
Maina Wanjau <mwanjau@safaricom.co.ke>
2023-05-25 15:10:45 +0300

    cache

--- main_app/settings/local.py
+++ main_app/settings/local.py
@@ -11,10 +11,16 @@ CACHE_MIDDLEWARE_KEY_PREFIX = 'mlb'
 
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
-        'LOCATION': '127.0.0.1:11211',
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379',
     }
 }
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
+#         'LOCATION': '127.0.0.1:11211',
+#     }
+# }
 
 # X_FRAME_OPTIONS= 'SAMEORIGIN'
 CORS_ALLOW_HEADERS = ['*']
