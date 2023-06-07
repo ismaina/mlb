6802a1cdec9ea3cc9d593c247c4314c93fe37be6
Maina Wanjau <mwanjau@safaricom.co.ke>
2023-06-07 16:11:31 +0300

    aws

--- docker/ec2-deploy.sh
+++ docker/ec2-deploy.sh
@@ -6,19 +6,21 @@ then
     exit 0
 fi
 
-git archive --format tar --output ./project.tar prod
+git archive --format tar --output ./mlb.tar aws
 
-echo "Uploading the project.....:-)...Be Patient!"
-rsync -av -e "ssh -i ~/.ssh/MainaEC2KP.pem" ./project.tar ec2-user@$ec2_IP_ADDRESS:/home/ec2-user/project.tar
-# rsync -avzhe ssh --progress ./project.tar mwanjau@$ec2_IP_ADDRESS:/home/mwanjau/tracker/project.tar
+echo "Uploading the mlb.....:-)...Be Patient!"
+rsync -av -e "ssh -i ~/.ssh/Maina.pem" ./mlb.tar ubuntu@$ec2_IP_ADDRESS:/home/ubuntu/Documents/mlb.tar
+# rsync -avzhe ssh --progress ./mlb.tar mwanjau@$ec2_IP_ADDRESS:/home/mwanjau/tracker/mlb.tar
 echo "Upload complete....:-)"
 
 
 echo "Building the image......."
-# ssh -i .ssh/MainaEC2KP.pem ec2-user@$ec2_IP_ADDRESS << 'ENDSSH'
-ssh mwanjau@$ec2_IP_ADDRESS << 'ENDSSH'
-    mkdir -p /thkisilon_container_nfs/tracker
-    rm -rf /thkisilon_container_nfs/tracker/* && tar -xf tracker/project.tar -C /thkisilon_container_nfs/tracker/
-    docker-compose -f /thkisilon_container_nfs/tracker/production.yml build
+# ssh mwanjau@$ec2_IP_ADDRESS << 'ENDSSH'
+ssh -i ~/.ssh/Maina.pem ubuntu@$ec2_IP_ADDRESS << 'ENDSSH'
+    cd Documents
+    mkdir -p mlb
+    rm -rf mlb/* && tar -xf mlb.tar -C mlb/
+    cd mlb
+    docker-compose -f local.yml build
 ENDSSH
 echo "Build completed successfully.......:-)"
