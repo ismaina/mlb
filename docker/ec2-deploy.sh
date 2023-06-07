5ee2ebc99e2da5c62b308a8171de9c5d367e39e5
Maina Wanjau <mwanjau@safaricom.co.ke>
2023-06-07 10:28:11 +0300

    working well

--- docker/ec2-deploy.sh
+++ docker/ec2-deploy.sh
@@ -6,10 +6,10 @@ then
     exit 0
 fi
 
-git archive --format tar --output ./project.tar swarm
+git archive --format tar --output ./project.tar prod
 
 echo "Uploading the project.....:-)...Be Patient!"
-rsync -av -e "ssh -i .ssh/MainaEC2KP.pem" ./project.tar ec2-user@$ec2_IP_ADDRESS:/home/ec2-user/project.tar
+rsync -av -e "ssh -i ~/.ssh/MainaEC2KP.pem" ./project.tar ec2-user@$ec2_IP_ADDRESS:/home/ec2-user/project.tar
 # rsync -avzhe ssh --progress ./project.tar mwanjau@$ec2_IP_ADDRESS:/home/mwanjau/tracker/project.tar
 echo "Upload complete....:-)"
 
