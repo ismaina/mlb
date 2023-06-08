72eeaf5c739a0cd3ba2218d25f357225963be191
Maina Wanjau <mwanjau@safaricom.co.ke>
2023-06-08 15:03:20 +0300

    trying to serve media files

--- main_app/urls.py
+++ main_app/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
         {"sitemaps": {"products": ProductSitemap}},
     ),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if bool(settings.DEBUG):
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
